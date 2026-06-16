88a9b75f6446ead99c38a1ea10b2621d30b5fd7f
pavsoss <pavsafterdark@gmail.com>
2026-06-16 17:42:09 +0530

    ruff format

--- agentwatch/alerting/engine.py
+++ agentwatch/alerting/engine.py
@@ -105,9 +105,9 @@ class AlertingEngine:
         from agentwatch.security.webhook_signing import generate_webhook_signature
 
         try:
-            payload_bytes = json.dumps(
-                payload, separators=(",", ":"), sort_keys=True
-            ).encode("utf-8")
+            payload_bytes = json.dumps(payload, separators=(",", ":"), sort_keys=True).encode(
+                "utf-8"
+            )
             headers = {"Content-Type": "application/json"}
             if self._config.webhook_signing_secret:
                 headers["X-AgentWatch-Signature"] = generate_webhook_signature(
